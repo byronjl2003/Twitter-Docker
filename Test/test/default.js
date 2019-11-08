8f8b7e7eb51f371390f314fd450c4ef8146e3e29
nowell <jorged104@gmail.com>
2019-11-08 02:21:00 -0600

    Todas las pruebas

--- Test/test/default.js
+++ Test/test/default.js
@@ -7,17 +7,77 @@ let service = new chrome.ServiceBuilder(geckoDriverPath ).build();
 chrome.setDefaultService(service);
 
 
-describe('DefaultTest', () => {
+describe('Test 1', () => {
     let driver =  new Builder().withCapabilities(Capabilities.chrome()).build();
    
-        it('should go to nehalist.io and check the title' , async() => {
+        it('Autentificacion correcta' , async() => {
     try {       
-        await driver.get('https://www.google.com');
-        const title = await driver.getTitle();
+        await driver.get('http://130.211.112.112');
+        await driver.findElement(By.name('usr')).sendKeys('@jorge')
+        await driver.findElement(By.name('pass')).sendKeys('asd',Key.ENTER)
+        await driver.sleep(1000);
+        var promise  = await driver.findElement(By.name('usr')).getAttribute("value")
+        .then((value) => { return value; });;
 
-        expect(title).to.equal('Google');
+        expect(promise).to.equal('@jorge');
+       
+       
      } finally {
-        await driver.quit();
+        //after(async () => driver.quit());
      }
     });
 });
+
+describe('Test 2', () => {
+    let driver =  new Builder().withCapabilities(Capabilities.chrome()).build();
+   
+        it('Autentificacion correcta' , async() => {
+    try {       
+        await driver.get('http://130.211.112.112');
+        await driver.findElement(By.name('usr')).sendKeys('@chino')
+        await driver.findElement(By.name('pass')).sendKeys('qwe',Key.ENTER)
+        await driver.sleep(1000);
+        var promise  = await driver.findElement(By.xpath('//h1')).getAttribute("innerText")
+        .then((value) => { return value; });;
+
+        expect(promise).to.equal('Password incorrecta');
+       
+       
+     } finally {
+        //after(async () => driver.quit());
+     }
+    });
+});
+
+describe('Test 3', () => {
+    let driver =  new Builder().withCapabilities(Capabilities.chrome()).build();
+   
+        it('Autentificacion correcta' , async() => {
+    try {       
+        await driver.get('http://130.211.112.112');
+        await driver.findElement(By.name('usr')).sendKeys('@jorge')
+        await driver.findElement(By.name('pass')).sendKeys('asd',Key.ENTER)
+        await driver.sleep(1000);
+        var promise  = await driver.findElement(By.xpath('//h3')).getAttribute("innerText")
+        .then((value) => { 
+            let val = value.toString().split(' '); 
+            return Number(val[0].trim());
+        });
+        await driver.sleep(1000);
+        await driver.findElement(By.name('txt')).sendKeys('holamundo',Key.ENTER)
+        await driver.sleep(1000);
+        var promise2  = await driver.findElement(By.xpath('//h3')).getAttribute("innerText")
+        .then((value) => { 
+            let val = value.toString().split(' '); 
+            return Number(val[0].trim());
+        });
+
+        expect(promise2).to.equal(promise+1);
+       
+       
+     } finally {
+        //after(async () => driver.quit());
+     }
+    });
+});
+
